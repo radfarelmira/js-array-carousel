4fa81000852d0e5d472428e1fbf26ecd5bb11998
radfarelmira <radfar.elmira@email.com>
2021-11-18 15:46:13 +0100

    milestone-2 scaffolding

---
+++ milestone-2/js/script.js
@@ -0,0 +1,30 @@
+// MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo le immagini e i testi delle immagini 
+// dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template 
+// literal. Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente 
+// attraverso JavaScript.
+
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
